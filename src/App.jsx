9970198d97b258f0ecd36038a9a83ff3406ae85a
unknown <l19350325@tuxtepec.tecnm.mx>
2023-03-29 12:54:33 -0600

    carrito de compra

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='container text-center'>
+      <div className='row'>
+        <div className='col'>
+        <h1>Shopping List</h1>
+        <hr />
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-8'>
+        Primer elemento de mi lista
+        </div>
+        <div className='col'>
+        Botones
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-8'>
+        Segundo elemento de mi lista
+        </div>
+        <div className='col'>
+        Botones
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
